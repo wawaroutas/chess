6df1c7e1ae99a1a371ad96df324479b7d7a5d3f0
wawaroutas <alexvava@csd.auth.gr>
2021-09-18 21:15:20 +0300

    added operator == to Position

--- src/position.h
+++ src/position.h
@@ -3,7 +3,11 @@
 
 struct Position {
  public:
+  Position() = default;
   int x, y;
+  bool operator==(const Position& other) const {
+    return x == other.x && y == other.y;
+  }
 };
 
 #endif /* CHESS_SRC_POSITION_H  */
