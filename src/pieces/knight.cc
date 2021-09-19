fd5fef0469de4a56fa7611e241b4e7ae60fb8f86
wawaroutas <alexvava@csd.auth.gr>
2021-09-19 20:01:54 +0300

    Implemented new design of AvailableMoves for all pieces

--- src/pieces/knight.cc
+++ src/pieces/knight.cc
@@ -13,19 +13,18 @@ Knight::Knight(Position initPostion, Color color)
 //can attack/move
 std::vector<Position>
 Knight::AvailableMoves(const std::vector<Material*>& enemy) const {
+  static const int kMovableDirections = 8;
+  static const Position moves[kMovableDirections] = {
+    {2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}
+  };
 	std::vector<Position> available;
-  Position moves[] = {{2,1},{2,-1},{-2,1},{-2,-1},{1,2},{-1,2},{1,-2},{-1,-2}};
-  Position tempPosition;
-  for(Position move : moves){
-    tempPosition = position_ + move;
-    if(tempPosition.InBoard()){
-      available.push_back(tempPosition);
-      Color c;
-      if(PositionOccupied(tempPosition,enemy,c) && c==GetColor()) //Ally occupied
-        available.pop_back();              //If its occupied by an ally piece
-    }                                    // remove the position from availables
+  for (Position move : moves) {
+    Position possible_position = position_ + move;
+    if (PositionValid(possible_position, enemy, color_)) {
+      available.push_back(possible_position);
+    }
   }
-	return available;
+  return available;
 }
 
 int Knight::value() const noexcept {
--- src/pieces/rook.cc
+++ src/pieces/rook.cc
@@ -12,109 +12,18 @@ Rook::Rook(Position initPostion, Color color)
 //piece collision and ally collision
 std::vector<Position>
 Rook::AvailableMoves(const std::vector<Material*>& enemy) const {
-	std::vector<Position> available;
-<<<<<<< HEAD
-  static const Position moves[] = {
-    {1,0}, {0,1}, {-1,0}, {0,-1}
+  static const int kMovableDirections = 4;
+  static const Position moves[kMovableDirections] = {
+    {1, 0}, {0, 1}, {0, -1}, {-1, 0}
   };
-
-
-  for(Position move : moves)
-  {
-    for(int i = 0 ; i < 4 ; ++i)
-=======
-	Position tempPosition;
-	int directions[2] = {-1, 1};
-	for(int dir : directions) //up and down movement
-  {
-  	tempPosition = position_;
-    tempPosition.x += dir;
-		while(tempPosition.InBoard())
-    {
-      Color c;
-      if(PositionOccupied(tempPosition,enemy,c))
-      {
-        if(GetColor()==c)
-          break; //Occupied by ally then change direction
-        else
-        {
-          available.push_back(tempPosition); //occupied by enemy then add it
-          break;                             // and change direction
-        }
-      }
-      else  //Not occupied at all
-        available.push_back(tempPosition);
-        tempPosition.x+=dir;
-    }
-	}
-  for(int dir : directions) //Left and right movement
-  {
-    tempPosition = position_;
-    tempPosition.y += dir;
-    while(tempPosition.InBoard())
->>>>>>> d120e8c9509e568cd5d5631379a96831fb3b6308
-    {
-      if(InBoard(position_+move))
-      {
-          available.push_back(position_+move);
-          Color c;
-          if(PositionOccupied(position_,enemy,c) && c == GetColor()) //same color
-          {
-            available.pop_back();
-            break;
-          }else
-          {
-            move+=move;
-          }
-      }
+	std::vector<Position> available;
+  for (Position move : moves) {
+    Position possible_position = position_;
+    while (PositionValid(possible_position += move, enemy, color_)) {
+      available.push_back(possible_position);
     }
   }
-	// int directions[2] = {-1, 1};
-	// for(int dir : directions) //up and down movement
-  // {
-  // 	tempPosition = position_;
-  //   tempPosition.x += dir;
-	// 	while(InBoard(tempPosition))
-  //   {
-  //     Color c;
-  //     if(PositionOccupied(tempPosition,enemy,c))
-  //     {
-  //       if(GetColor()==c)
-  //         break; //Occupied by ally then change direction
-  //       else
-  //       {
-  //         available.push_back(tempPosition); //occupied by enemy then add it
-  //         break;                             // and change direction
-  //       }
-  //     }
-  //     else  //Not occupied at all
-  //       available.push_back(tempPosition);
-  //       tempPosition.x+=dir;
-  //   }
-	// }
-  // for(int dir : directions) //Left and right movement
-  // {
-  //   tempPosition = position_;
-  //   tempPosition.y += dir;
-  //   while(InBoard(tempPosition))
-  //   {
-  //     Color c;
-  //     if(PositionOccupied(tempPosition,enemy,c))
-  //     {
-  //       if(GetColor()==c)
-  //         break; //Occupied by ally then change direction
-  //       else
-  //       {
-  //         available.push_back(tempPosition); //occupied by enemy then add it
-  //         break;                             // and change direction
-  //       }
-  //     }
-  //     else  //Not occupied at all
-  //       available.push_back(tempPosition);
-  //       tempPosition.y+=dir;
-  //   }
-  // }
-	return available;
+  return available;
 }
 
 int Rook::value() const noexcept {
