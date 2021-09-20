fd8ed35645d9cb4bd795ad1beb264329bb7498e1
Yosoku <Edouardos@csd.auth.gr>
2021-09-20 14:30:35 +0300

    Overloads operator<< in Color & Location.Added square.cc

--- src/Board/square.h
+++ src/Board/square.h
@@ -3,15 +3,25 @@
 
 #include "color.h"
 #include "location.h"
+#include "piece.h"
+#include <iostream>
 
 class Square
 {
 private:
-  Location location_;
-  Color color_;
-  Piece* piece;
-  bool occupied_;
+  const Location location_;
+  const Color color_;
+  Piece* piece_;
+public:
+  Square(Location location,Color color,Piece* piece);
+  Location GetLocation() const;
+  Color GetColor() const;
+  Piece* GetPiece();
+  bool Occupied();
+  bool operator==(const Square& other);
+  friend std::ostream& operator<<(std::ostream&, Square&);
 };
+
 
 
 #endif // CHESS_SRC_BOARD_SQUARE_H
