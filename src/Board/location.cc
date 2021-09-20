d36b7a376ef81b4ff966c548960d859d62a1f9b9
wawaroutas <alexvava@csd.auth.gr>
2021-09-20 14:32:20 +0300

    formatting in location class

--- src/Board/location.cc
+++ src/Board/location.cc
@@ -1,14 +1,21 @@
 #include "Location.h"
-#include <string> //std::string
-#include <map> //std::map
 
-Location::Location(BoardFile file,int rank) : file_(file), rank_(rank){}
+#include <string> // std::string, std::to_string()
+#include <map>    // std::map
 
-std::string Location::toString()
-{
-  static std::map<BoardFile,std::string> strings= {{BoardFile::A,"A"},
-  {BoardFile::B,"B"},{BoardFile::C,"C"},{BoardFile::D,"D"},{BoardFile::E,"E"},
-  {BoardFile::F,"F"},{BoardFile::G,"G"},{BoardFile::H,"H"},};
+Location::Location(BoardFile file, int rank) : file_(file), rank_(rank){}
+
+std::string Location::toString() {
+  static std::map<BoardFile, std::string> strings = {
+    {BoardFile::A, "A"},
+    {BoardFile::B, "B"},
+    {BoardFile::C, "C"},
+    {BoardFile::D, "D"},
+    {BoardFile::E, "E"},
+    {BoardFile::F, "F"},
+    {BoardFile::G, "G"},
+    {BoardFile::H, "H"}
+  };
   return strings[file_] + std::to_string(rank_);
 }
 
