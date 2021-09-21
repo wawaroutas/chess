820afece0988d932f24d3dde5bbfa65d012bbe7c
Yosoku <Edouardos@csd.auth.gr>
2021-09-21 22:18:30 +0300

    Added setters for name and pieces in Player class,added default constructor

--- src/Application/player.cc
+++ src/Application/player.cc
@@ -3,15 +3,11 @@
 #include <vector> //std::erase, std::begin/end
 #include <algorithm> // std::remove
 
+Player::Player(): name_(0),pieces_({}),points_(0){}
 
+Player::Player(const char* name,std::vector<Piece*> pieces)
+:name_(name),pieces_(pieces),points_(0){}
 
-Player::Player(const char* name,std::vector<Piece*> pieces):name_(name),pieces_(pieces),
-points_(0){}
-
-//---------Getters--------/
-std::vector<Piece*> Player::GetPieces() const noexcept{return pieces_;}
-int Player::GetPoints() const noexcept {return points_;}
-const char* Player::GetName() const noexcept {return name_;}
 
 
 void Player::AddPoints(int points){
@@ -23,3 +19,11 @@ void Player::RemovePiece(Piece* piece)
   pieces_.erase(std::remove(std::begin(pieces_),std::end(pieces_) ,piece)
   ,std::end(pieces_));
 }
+
+
+//---------Getters and Setters--------/
+std::vector<Piece*> Player::GetPieces() const noexcept{return pieces_;}
+int Player::GetPoints() const noexcept {return points_;}
+const char* Player::GetName() const noexcept {return name_;}
+void Player::SetName(const char* newName){name_ = newName;}
+void Player::SetPieces(std::vector<Piece*> pieces){pieces_ = pieces;}
